a5d742e0ecb087d79da97e0c7e9ca0bbfeb7304f
unknown <hiago7mg@gmail.com>
2019-09-11 00:44:36 -0300

    Create scoreboard project

---
+++ scoreboard/src/main/java/com/softwarrior/scoreboard/ScoreboardApplication.java
@@ -0,0 +1,13 @@
+package com.softwarrior.scoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreboardApplication.class, args);
+    }
+
+}
