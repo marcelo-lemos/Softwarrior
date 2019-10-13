43e572e3528fad83142e6032158d99087cc61bc5
Marcelo Lemos <marceloharrylemos@gmail.com>
2019-10-13 19:19:47 -0300

    Add pause menu interface

--- game/Nodes/Stages/Pause.gd
+++ game/Nodes/Stages/Pause.gd
@@ -6,3 +6,21 @@ func _input(event):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
+		if visible == true:
+			$VBoxContainer/Button.grab_focus()
+
+func _on_Button_pressed():
+	get_tree().paused = false
+	visible = false
+
+
+func _on_Button2_pressed():
+	pass
+
+
+func _on_Button3_pressed():
+	get_tree().change_scene("Nodes/UI/TitleScreen.tscn")
+
+
+func _on_Button4_pressed():
+	get_tree().quit()
