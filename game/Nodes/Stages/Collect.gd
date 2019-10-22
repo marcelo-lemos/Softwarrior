77d851b3f2c7814e87a1ebf6f81de98fbe7c5ce5
unknown <hiago7mg@gmail.com>
2019-10-22 04:49:22 -0300

    Adjust volume on sound effects

--- game/Nodes/Stages/Collect.gd
+++ game/Nodes/Stages/Collect.gd
@@ -3,7 +3,8 @@ signal collected;
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var animation_finished = false
+var audio_finished = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -20,8 +21,16 @@ func _on_Area2D_body_entered(body):
 	$CollisionShape2D.set_deferred("disabled",true)
 
 func _on_Sprite_animation_finished():
-	pass
+	if $Sprite.animation == "OnPickup":
+		if audio_finished:
+			queue_free()
+		else:
+			animation_finished = true
+			$Sprite.visible = false
 
 
 func _on_AudioStreamPlayer_finished():
-	queue_free()
+	if animation_finished:
+		queue_free()
+	else:
+		audio_finished = true
