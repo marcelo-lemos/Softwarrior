1456499e448d7a7fa0b271e2a5cb5c4608e8f47b
unknown <hiago7mg@gmail.com>
2019-10-23 01:15:25 -0300

    Fix air attack Add air attack animation Add VFlier animation

--- game/Nodes/Characters/Player/AirAttack.gd
+++ game/Nodes/Characters/Player/AirAttack.gd
@@ -4,21 +4,24 @@ var canceled = false
 var attack_type =  {'damage': 1, 'animation': 'air_attack'}
 
 func enter():
-	canceled = true
+	canceled = false
 	print("Entered AIR_ATTACK")
-	sprite.play("Idle")
+	sprite.play("AirAttack")
 	player.katana.attack(attack_type)
 	
 func update(delta):
 	if canceled:
 		emit_signal("finished", "previous")
-	
 	player.velocity.y += GRAVITY
+	
 	apply_air_slowdown()
 	
 	if(Input.is_action_just_released("jump")):
 		check_jump_cut(jump_var_height)
 	
+	if body.is_on_floor():
+		emit_signal("finished", "move")
+		
 	if Input.is_action_pressed("dash") and player.has_dash:
 		canceled = true
 		emit_signal("finished", "dash")
--- game/Nodes/Characters/Player/PlayerStateMachine.gd
+++ game/Nodes/Characters/Player/PlayerStateMachine.gd
@@ -15,7 +15,7 @@ func _change_state(state_name):
 	if not _active:
 		return
 
-	if state_name in ["dash_attack", "on_wall", "mid_air", "dash", "ground_attack", "air_attack"]:
+	if state_name in ["move","dash_attack", "on_wall", "mid_air", "dash", "ground_attack", "air_attack"]:
 		print(">>>>> " + states_map[state_name].name)
 		states_stack.push_front(states_map[state_name])
 
